2cde7abc4f541cc46428c7a528e7caf506752c42
yungshun317 <seraph317@gmail.com>
2024-05-20 04:28:34 +0800

    [FEAT] Use `MeshWobbleMaterial` which makes the geometry wobble and wave around

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,6 +1,18 @@
+import {Environment, MeshWobbleMaterial, OrbitControls} from "@react-three/drei";
+
 const Scene = () => {
     return (
-        <></>
+        <>
+            <OrbitControls/>
+            <ambientLight/>
+
+            <Environment background files="./static/env-maps/1.hdr" />
+
+            <mesh>
+                <boxGeometry args={[1, 1, 1, 32, 32, 32]} />
+                <MeshWobbleMaterial color="#F76E53" factor={3} speed={0.4} />
+            </mesh>
+        </>
     );
 };
 
