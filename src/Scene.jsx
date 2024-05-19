12ded2c47709f3cd16a8fbba1aa811e1a9fa62c3
yungshun317 <seraph317@gmail.com>
2024-05-20 04:33:14 +0800

    [FEAT] Use `MeshReflectorMaterial` which adds reflections and blur to any mesh

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,4 +1,4 @@
-import {Environment, MeshWobbleMaterial, OrbitControls} from "@react-three/drei";
+import {Environment, MeshReflectorMaterial, MeshWobbleMaterial, OrbitControls} from "@react-three/drei";
 
 const Scene = () => {
     return (
@@ -8,9 +8,20 @@ const Scene = () => {
 
             <Environment background files="./static/env-maps/1.hdr" />
 
-            <mesh>
+            { /* <mesh>
                 <boxGeometry args={[1, 1, 1, 32, 32, 32]} />
                 <MeshWobbleMaterial color="#F76E53" factor={3} speed={0.4} />
+            </mesh> */ }
+
+            <mesh rotation-x={-Math.PI * 0.5} position-y={-0.75}>
+                <planeGeometry args={[6, 6]} />
+                <MeshReflectorMaterial
+                    resolution={512}
+                    color="gray"
+                    blur={[1000, 1000]}
+                    mixBlur={1}
+                    mirror={1}
+                />
             </mesh>
         </>
     );
