b07b131e49d46ec0fd7160c3bb5bb4dece00bace
yungshun317 <seraph317@gmail.com>
2024-05-20 04:43:01 +0800

    [FEAT] Use `MeshDistortMaterial` and make the geometry distort with the `useCursor` hook

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,20 +1,39 @@
-import {Environment, MeshReflectorMaterial, MeshWobbleMaterial, OrbitControls} from "@react-three/drei";
+import {
+    Environment,
+    GradientTexture, MeshDistortMaterial,
+    MeshReflectorMaterial,
+    MeshWobbleMaterial,
+    OrbitControls, useCursor
+} from "@react-three/drei";
+import {useEffect, useRef, useState} from "react";
 
 const Scene = () => {
+    const [hover, setHover] = useState(false);
+    const planeRef = useRef();
+    useCursor(hover);
+
+    useEffect(() => {
+        if (hover) {
+            planeRef.current.material.distort = 0.4;
+        } else {
+            planeRef.current.material.distort = 0;
+        }
+    }, [hover]);
+
     return (
         <>
             <OrbitControls/>
             <ambientLight/>
 
-            <Environment background files="./static/env-maps/1.hdr" />
+            <Environment background files="./static/env-maps/1.hdr"/>
 
             { /* <mesh>
                 <boxGeometry args={[1, 1, 1, 32, 32, 32]} />
                 <MeshWobbleMaterial color="#F76E53" factor={3} speed={0.4} />
-            </mesh> */ }
+            </mesh> */}
 
-            <mesh rotation-x={-Math.PI * 0.5} position-y={-0.75}>
-                <planeGeometry args={[6, 6]} />
+            { /* <mesh rotation-x={-Math.PI * 0.5} position-y={-0.75}>
+                <planeGeometry args={[6, 6]}/>
                 <MeshReflectorMaterial
                     resolution={512}
                     color="gray"
@@ -22,6 +41,17 @@ const Scene = () => {
                     mixBlur={1}
                     mirror={1}
                 />
+            </mesh> */}
+
+            <mesh
+                ref={planeRef}
+                onPointerOver={() => setHover(true)}
+                onPointerOut={() => setHover(false)}
+            >
+                <planeGeometry args={[2, 3, 64, 64]} />
+                <MeshDistortMaterial speed={3} distort={0}>
+                    <GradientTexture colors={["aquamarine", "hotpink"]} stops={[0, 1]} />
+                </MeshDistortMaterial>
             </mesh>
         </>
     );
